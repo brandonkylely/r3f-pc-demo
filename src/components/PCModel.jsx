68e2fcc093c346cd738616bd65d417d6660bd2f3
Brandon <brandonkly@ucla.edu>
2024-06-30 16:50:07 -0700

    fixed deployment issues

--- src/components/PCModel.jsx
+++ src/components/PCModel.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import pc from '../assets/pcopen.glb'
+import pc from '/assets/pcopen.glb'
 import { OrbitControls, useGLTF } from '@react-three/drei'
 
 function PCModel(props) {
