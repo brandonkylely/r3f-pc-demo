1dd48d0dc7443264c6bc9f6db0cc332fb3a5b2b7
Brandon <brandonkly@ucla.edu>
2024-06-30 17:17:31 -0700

    loading screen style

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import { useProgress, Html } from "@react-three/drei";
 export default function Loading() {
   const { progress } = useProgress();
   return (
-    <Html wrapperClass="itimFont" center>
+    <Html wrapperClass="loading" center>
       <div class="lds-ellipsis">
         <div></div>
         <div></div>
